a146c07af77ca9f20ac66682676e3622d7b49e46
Evan sir <danolevan@gmail.com>
2020-02-11 11:59:39 +0545

    fir

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //operators
+
+            //arithematic operator  + - / *  %
+            //relational == < > <= >= !=
+            //logical and or not && || ! 
+            //bitwise  &  | ~  ^
+            //assignment = += -= *= /= %=
+
+            // operand operator
+
+
+
+            //if(5>10 || 6<=3)
+            //{
+            //    Console.WriteLine("if");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("else");
+            //}
+
+
+
+
+            //int a = 3;
+            //int b = 4;
+
+            //if(a!=b)
+            //{
+            //    Console.WriteLine("if");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("else");
+            //}
+
+
+            //if (!(5 < 10))
+            //{
+            //    Console.WriteLine("if");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("else");
+            //}
+
+            //  Console.WriteLine(3^9);
+
+            //  11
+            //1001
+            //---
+            // 1010
+
+            //int a = 5;
+            //a += 5;   // a=a+5;
+
+            //a -= 5; // a=a-5;
+
+            //a *= 5; // a= a*5;
+            //a /= 5;
+            //a %= 3;// a= a % 5;
+            //Console.WriteLine(a);
+
+
+        }
+    }
+}
