b75fefd405b8461480433101f43573be9b722389
Evan sir <danolevan@gmail.com>
2020-02-12 12:02:36 +0545

    erwe

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,100 +8,141 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            //operators
+            //loop
 
-            //arithematic operator  + - / *  %
-            //relational == < > <= >= !=
-            //logical and or not && || ! 
-            //bitwise  &  | ~  ^
-            //assignment = += -= *= /= %=
+            // for while do  
 
-            // operand operator
+            //1 initialization  a=1
+            //2 condition    a<10
+            //3 increment/decrement  a=a+1 a=a-1
 
-
-
-            //if(5>10 || 6<=3)
+            //for(int a=1;a<=10;a=a+1)
             //{
-            //    Console.WriteLine("if");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("else");
+            //    Console.WriteLine(a);
             //}
 
-
-
-
-            //int a = 3;
-            //int b = 4;
-
-            //if(a!=b)
+            //for (int a = 10; a >= 1; a = a - 1)
             //{
-            //    Console.WriteLine("if");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("else");
+            //    Console.WriteLine(a);
+            //    Console.WriteLine("hi");
             //}
 
-
-            //if (!(5 < 10))
+            //int a = 1;
+            //while(a<=10)
             //{
-            //    Console.WriteLine("if");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("else");
+            //    Console.WriteLine(a);
+            //    a = a + 1;
             //}
 
-            //  Console.WriteLine(3^9);
+            //int a = 1;
 
-            //  11
-            //1001
-            //---
-            // 1010
+            //do
+            //{
+            //    Console.WriteLine(a);
+            //    a = a + 1;
+            //} while (a <= 10);
 
-            //int a = 5;
-            //a += 5;   // a=a+5;
-
-            //a -= 5; // a=a-5;
-
-            //a *= 5; // a= a*5;
-            //a /= 5;
-            //a %= 3;// a= a % 5;
-            //Console.WriteLine(a);
-
-            int a = 5;
-
-            if(a==5)
-            {
-
-            }
-            else if(a==6)
-            {
-
-            }
-            else if(a==7)
-            {
-
-            }
-            else
-            {
-
-            }
-            switch(a)
-            {
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                default:
-                    break;
-            }
         }
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    //operators
+
+        //    //arithematic operator  + - / *  %
+        //    //relational == < > <= >= !=
+        //    //logical and or not && || ! 
+        //    //bitwise  &  | ~  ^
+        //    //assignment = += -= *= /= %=
+
+        //    // operand operator
+
+
+
+        //    //if(5>10 || 6<=3)
+        //    //{
+        //    //    Console.WriteLine("if");
+        //    //}
+        //    //else
+        //    //{
+        //    //    Console.WriteLine("else");
+        //    //}
+
+
+
+
+        //    //int a = 3;
+        //    //int b = 4;
+
+        //    //if(a!=b)
+        //    //{
+        //    //    Console.WriteLine("if");
+        //    //}
+        //    //else
+        //    //{
+        //    //    Console.WriteLine("else");
+        //    //}
+
+
+        //    //if (!(5 < 10))
+        //    //{
+        //    //    Console.WriteLine("if");
+        //    //}
+        //    //else
+        //    //{
+        //    //    Console.WriteLine("else");
+        //    //}
+
+        //    //  Console.WriteLine(3^9);
+
+        //    //  11
+        //    //1001
+        //    //---
+        //    // 1010
+
+        //    //int a = 5;
+        //    //a += 5;   // a=a+5;
+
+        //    //a -= 5; // a=a-5;
+
+        //    //a *= 5; // a= a*5;
+        //    //a /= 5;
+        //    //a %= 3;// a= a % 5;
+        //    //Console.WriteLine(a);
+
+        //    //int a = 5;
+
+        //    //if(a==5)
+        //    //{
+
+        //    //}
+        //    //else if(a==6)
+        //    //{
+
+        //    //}
+        //    //else if(a==7)
+        //    //{
+
+        //    //}
+        //    //else
+        //    //{
+
+        //    //}
+        //    //switch(a)
+        //    //{
+        //    //    case 5:
+        //    //        break;
+        //    //    case 6:
+        //    //        break;
+        //    //    case 7:
+        //    //        break;
+        //    //    default:
+        //    //        break;
+        //    //}
+        //}
     }
 }
