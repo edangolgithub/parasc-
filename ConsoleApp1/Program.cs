c2ce3a8f8a2b1cfabb38bcd723d8da5f168df283
Evan sir <danolevan@gmail.com>
2020-02-12 12:26:13 +0545

    erfwertf

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,44 +8,103 @@ namespace ConsoleApp1
 {
     class Program
     {
-
         static void Main()
         {
-            //loop
+            Console.WriteLine("enter a number for multiplication table");
+            string a=Console.ReadLine();
+            int num = Convert.ToInt32(a);
 
-            // for while do  
-
-            //1 initialization  a=1
-            //2 condition    a<10
-            //3 increment/decrement  a=a+1 a=a-1
-
-            //for(int a=1;a<=10;a=a+1)
-            //{
-            //    Console.WriteLine(a);
-            //}
-
-            //for (int a = 10; a >= 1; a = a - 1)
-            //{
-            //    Console.WriteLine(a);
-            //    Console.WriteLine("hi");
-            //}
-
-            //int a = 1;
-            //while(a<=10)
-            //{
-            //    Console.WriteLine(a);
-            //    a = a + 1;
-            //}
-
-            //int a = 1;
-
-            //do
-            //{
-            //    Console.WriteLine(a);
-            //    a = a + 1;
-            //} while (a <= 10);
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}",num,i,num*i);
+            }
 
         }
+
+        //static void Main()
+        //{
+        //    for (int i = 1; i <= 10; i++)
+        //    {
+        //        Console.WriteLine(i * i * i);
+        //    }
+        //}
+        //static void Main()
+        //{
+        //    int sum = 0;
+        //    for (int i = 1; i < 8; i++)
+        //    {
+        //        Console.WriteLine(i);
+        //        sum = sum + i;
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+
+        //static void Main()
+        //{
+        //    //1 2 3 4 5 6 7 8 9 10
+        //    int sum = 1;
+        //    for(int i=1;i<=10;i++)
+        //    {
+        //        sum = sum * i;
+        //    }
+        //    Console.WriteLine(sum);
+        //}
+
+        //static void Main()
+        //{
+        //    // 2 4 6 8 10
+        //    // 1 3 5 7 9
+
+        //    for(int i=1; i<=10;i=i+1)
+        //    {
+        //        if (i % 2 == 1)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+
+        //    }
+        //}
+
+
+
+
+        //static void Main()
+        //{
+        //    //loop
+
+        //    // for while do  
+
+        //    //1 initialization  a=1
+        //    //2 condition    a<10
+        //    //3 increment/decrement  a=a+1 a=a-1
+
+        //    //for(int a=1;a<=10;a=a+1)
+        //    //{
+        //    //    Console.WriteLine(a);
+        //    //}
+
+        //    //for (int a = 10; a >= 1; a = a - 1)
+        //    //{
+        //    //    Console.WriteLine(a);
+        //    //    Console.WriteLine("hi");
+        //    //}
+
+        //    //int a = 1;
+        //    //while(a<=10)
+        //    //{
+        //    //    Console.WriteLine(a);
+        //    //    a = a + 1;
+        //    //}
+
+        //    //int a = 1;
+
+        //    //do
+        //    //{
+        //    //    Console.WriteLine(a);
+        //    //    a = a + 1;
+        //    //} while (a <= 10);
+
+        //}
 
 
 
